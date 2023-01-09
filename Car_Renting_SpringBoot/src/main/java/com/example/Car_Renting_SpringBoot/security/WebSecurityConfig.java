e4d918c0d2c37d03fc9e8a3565c0239cbf774555
SI-W-00572\Si2001 <lorenzo.biraghi@si2001.it>
2023-01-09 10:06:27 +0100

    fix

--- Car_Renting_SpringBoot/src/main/java/com/example/Car_Renting_SpringBoot/security/WebSecurityConfig.java
+++ Car_Renting_SpringBoot/src/main/java/com/example/Car_Renting_SpringBoot/security/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import com.example.Car_Renting_SpringBoot.util.jwt.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,7 +13,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
